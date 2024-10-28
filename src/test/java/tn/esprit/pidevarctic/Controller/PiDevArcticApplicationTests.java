4110da95062644f2f0042ce060fef3f584227260
ghassenmassaoud <ghassen.massaoud@esprit.tn>
2024-10-29 00:43:49 +0100

    tests2

---
+++ src/test/java/tn/esprit/pidevarctic/Controller/PiDevArcticApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.pidevarctic.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiDevArcticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
